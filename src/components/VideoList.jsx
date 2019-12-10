45aa1dfb075003446674c129e8a8c2a9ddd1e372
kamalogudah <mcpaul2058@gmail.com>
2019-12-10 14:42:47 +0300

    Rendering a list of video Component

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import VideoItem from './videoItem';
 
-const VideoList = props =>  {
- 
-  return <div>{props.videos.length}</div>;
-  
+
+const VideoList = ({ videos }) => {
+
+  const renderedList = videos.map((video) => {
+    return <VideoItem />;
+   });
+  return <div>{ renderedList }</div>;
 }
 
 export default VideoList;
